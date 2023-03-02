743bf5243c27b9a978d104b213a79b3060898221
Brett Williamson <brett.williamson@rubiconwater.com>
2023-03-03 08:59:14 +1100

    Add authentication

--- stacks/index.js
+++ stacks/index.js
@@ -1,5 +1,6 @@
 import { StorageStack } from "./StorageStack";
 import { ApiStack } from "./ApiStack";
+import { AuthStack } from "./AuthStack";
 
 export default function main(app) {
   app.setDefaultFunctionProps({
@@ -9,5 +10,5 @@ export default function main(app) {
       format: "esm",
     },
   });
-  app.stack(StorageStack).stack(ApiStack);
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
 }
