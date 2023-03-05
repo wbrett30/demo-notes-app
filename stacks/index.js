16031811ef621e0551d9f7c33554ee9e2d78a2f0
Brett Williamson <brett.williamson@rubiconwater.com>
2023-03-05 11:25:03 +1100

    Configure app for backend access

--- stacks/index.js
+++ stacks/index.js
@@ -1,6 +1,7 @@
 import { StorageStack } from "./StorageStack";
 import { ApiStack } from "./ApiStack";
 import { AuthStack } from "./AuthStack";
+import { FrontendStack } from "./FrontendStack";
 
 export default function main(app) {
   app.setDefaultFunctionProps({
@@ -10,5 +11,5 @@ export default function main(app) {
       format: "esm",
     },
   });
-  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack).stack(FrontendStack);
 }
